e7869e7d3f4dd2f627cd1617c08e62f82ac8ce52
lupogryph <lupogryph@gmail.com>
2024-09-08 12:24:42 +0200

    fix node module + conf + user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [User],
+      synchronize: Boolean(process.env.DATABASE_SYNC) || false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,33 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity('user')
+@Unique(['email'])
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  prenom: string;
+
+  @Column()
+  nom: string;
+
+  @Exclude()
+  @Column()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: ['ADMIN', 'OPERATOR', 'INVITE'],
+    default: 'INVITE',
+  })
+  role: 'ADMIN' | 'OPERATOR' | 'INVITE';
+
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
+}
